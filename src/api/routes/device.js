ab758938fa93f97b39aa1c2a6351c8615b19c89d
SuperQuentin <quentin.aellen@cpnv.ch>
2021-01-24 16:38:32 +0100

    add all crud route with post validation system

---
+++ src/api/routes/device.js
@@ -0,0 +1,52 @@
+const Router = require('express').Router
+const deviceController = require('../controller/device')
+
+const { celebrate, Joi } = require('celebrate')
+
+const routes = Router()
+
+routes.get('/', (req, res) => {
+    deviceController.all(req,res)
+})
+
+routes.post('/', celebrate({
+    body : Joi.object({
+        deviceName: Joi.string().required(),
+        mqttClientId: Joi.string().required(),
+        description: Joi.string().optional(),
+        brand: Joi.string().optional(),
+        model: Joi.string().optional(),
+    })
+}),
+(req, res) => {
+    deviceController.create(req,res)
+})
+
+routes.post('/update', celebrate({
+    body : Joi.object({
+        id: Joi.string().required(),
+        deviceName: Joi.string().optional(),
+        mqttClientId: Joi.string().optional(),
+        description: Joi.string().optional(),
+        brand: Joi.string().optional(),
+        model: Joi.string().optional(),
+    })
+}),
+(req, res) => {
+    deviceController.update(req,res)
+})
+
+routes.get('/:id', (req, res) => {
+    deviceController.find(req,res)
+})
+
+routes.post('/remove',celebrate({
+    body : Joi.object({
+        id: Joi.string().required()
+    })
+}),
+(req, res) => {
+    deviceController.delete(req,res)
+})
+
+module.exports = routes
