ef6b04e076afb50351448ccf60852f5e36616fce
SuperQuentin <quentin.aellen@cpnv.ch>
2021-01-24 23:26:05 +0100

    remove random passwd gen

--- src/model/user.js
+++ src/model/user.js
@@ -1,12 +1,11 @@
 const mongoose = require('mongoose')
 
-let randPwd = Math.random().toString(36).slice(-8)
 
 const User = new mongoose.Schema(
   {
     _id: mongoose.Schema.Types.ObjectId,
-    username: { type: String, required: true },
-    password: { type: String, required: true, default: randPwd },
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: false },
     devices: [
         {
             type: mongoose.Schema.Types.ObjectId,
